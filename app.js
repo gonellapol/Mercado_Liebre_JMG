b10ef1bd121911516b0c6bfa0185846afde0b2c4
gonellapol <gonellapol@hotmail.com>
2022-08-29 20:56:11 -0300

    heroku data

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3001, ()=>{
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT || 3030, function(){
+    console.log("Servidor funcionando en puerto3030");
 });
 
 app.get('/', (req,res)=>{
